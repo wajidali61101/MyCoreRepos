d0edb2c9f2d41fcd7c1e22c63468e17a6f5b96bc
wajidali61101 <wajidali61101@outlook.com>
2022-01-31 11:24:32 +0500

    Initial Commit

---
+++ NetCoreGithubApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreGithubApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
